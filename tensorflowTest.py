605fb797022d8814ca4739c2a16909483f6ba405
Ole Petersen <56505957+peteole@users.noreply.github.com>
2020-08-01 16:53:09 +0200

    Add files via upload

---
+++ tensorflowTest.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+
+
+def f(x: tf.Variable) -> tf.Variable:
+    return x*x-2*x+1
+
+
+with tf.GradientTape(persistent=False) as tape:
+    trainable = tf.Variable(3.)
+    y=trainable*trainable
+    x1 = f(trainable)
+print(tape.gradient(x1, trainable))
+#print(tape.gradient(y,trainable))
