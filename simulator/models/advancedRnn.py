0be6ef95fc6e59363485df360534e64278b4490a
Ole Petersen <peteole2707@gmail.com>
2020-09-06 13:12:43 +0200

    added constants

--- simulator/models/advancedRnn.py
+++ simulator/models/advancedRnn.py
@@ -1,2 +1,6 @@
 from tools import readLog
 import tensorflow as tf
+from ..tools.modelToRnnCell import ModelRnn
+layers=tf.keras.layers
+def getModel():
+    input=layers.Input((INPUT_TIME_SERIES_LENGTH))
