1bffa9b98e62b5c76bb96200f88beff16a09fd54
Ole Petersen <peteole2707@gmail.com>
2020-09-06 16:54:24 +0200

    imporved model

--- simulator/main.py
+++ simulator/main.py
@@ -1,10 +1,6 @@
-from pathlib import Path
 from helper.tools import readLog, constants, predictNext
 import trainModel
 
-base_path = Path(__file__).parent
-file_path = (base_path / "../dataCollection/logs/flight2.txt").resolve()
-dataSet = readLog.FlightDataSet(file_path)
 
 SIZE = constants.INPUT_TIME_SERIES_LENGTH
 
--- simulator/trainModel.py
+++ simulator/trainModel.py
@@ -1,11 +1,11 @@
-from helper.models import advancedRnn as usedModel
+from helper.models import basicModel as usedModel
 import tensorflow as tf
 from pathlib import Path
 import numpy as np
 
 from helper.tools import constants, readLog, getSamples
 base_path = Path(__file__).parent
-file_path = (base_path / "../dataCollection/logs/flight2.txt").resolve()
+file_path = (base_path / "../dataCollection/logs/flight0.txt").resolve()
 dataSet = readLog.FlightDataSet(file_path)
 
 SIZE = constants.INPUT_TIME_SERIES_LENGTH
@@ -13,12 +13,12 @@ STEP = constants.STEP
 
 
 def train(loadBackup):
-    model = usedModel.getModel(SIZE, constants.PARAMETER_COUNT)
+    model = usedModel.getModel(SIZE)
 
     tf.keras.utils.plot_model(model)
     model.summary()
     model.compile(
-        optimizer=tf.keras.optimizers.Adam(),
+        optimizer=tf.keras.optimizers.Adam(0.0005),
         loss=tf.losses.mse
     )
 
@@ -33,7 +33,7 @@ def train(loadBackup):
             x=[xdata, ydata],
             y=labels,
             batch_size=50,
-            epochs=1000
+            epochs=5000
         )
         model.save_weights('./checkpoint/sim')
 
