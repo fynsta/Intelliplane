e643d5d9cd7f810beafff7c5232151d4587d635f
Fynn Starke <fynsta1904@gmail.com>
2020-09-06 15:51:07 +0200

    Predict Next

--- simulator/main.py
+++ simulator/main.py
@@ -1,35 +1,12 @@
-import tensorflow as tf
 from pathlib import Path
-import numpy as np
-
-from tools import readLog
+from tools import readLog, constants, predictNext
 import trainModel
+
 base_path = Path(__file__).parent
 file_path = (base_path / "../dataCollection/logs/flight2.txt").resolve()
 dataSet = readLog.FlightDataSet(file_path)
 
-SIZE = 20
-STEP = 0.1
-REDSIZE = 5
+SIZE = constants.INPUT_TIME_SERIES_LENGTH
 
 model = trainModel.train(True)
-
-def pred(elv, pitch):
-    elv = np.array([elv])
-    pitch = np.array([pitch])
-    res = model.predict([elv, pitch])
-    return np.ndarray.item(res)
-
-
-pitchSer = [0 for i in range(SIZE)]
-elvSer = [0.5 for i in range(SIZE)]
-while True:
-    next = pred(elvSer, pitchSer)
-    print(next)
-    pitchSer.pop(0)
-    elvSer.pop(0)
-    pitchSer.append(next)
-    elvSer.append(float(input()))
-quit()
-def loss(out, tar):
-    return tf.reduce_mean(tf.square(out-tar))
+predictNext.predictFromInput(SIZE, model)
