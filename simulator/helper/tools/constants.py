d8f5d434af6062cdc0d30ca0a393e0107e63441d
Ole Petersen <peteole2707@gmail.com>
2020-10-04 13:29:22 +0200

    commented constants

--- simulator/helper/tools/constants.py
+++ simulator/helper/tools/constants.py
@@ -1,13 +1,13 @@
 
 
 
-INPUT_TIME_SERIES_LENGTH = 25
-PARAMETER_COUNT = 5
-PREDICTABLE_PARAM_COUNT = 2
-CONTROLLABLE_PARAM_COUNT=PARAMETER_COUNT-PREDICTABLE_PARAM_COUNT
-STEP = 0.1
+INPUT_TIME_SERIES_LENGTH = 25 #length of history to predict from
+PARAMETER_COUNT = 5 # total number of controllable and measurable parameters
+PREDICTABLE_PARAM_COUNT = 2 #not controllable parameters (pitch, bank)
+CONTROLLABLE_PARAM_COUNT=PARAMETER_COUNT-PREDICTABLE_PARAM_COUNT #controllable parameters (throttle, elevator, aileron)
+STEP = 0.1 #timesteps to predict in seconds.
 
-MODEL = "rnnSimV2"
-LOADBACKUP = True
+MODEL = "rnnSimV2" #the only 100% working model by now
+LOADBACKUP = True # whether to load a pretrained model or not
 
-TRAINING_DATA=["flight0.txt", "flight3.txt"]
+TRAINING_DATA=["flight0.txt", "flight3.txt"] #flight logs to use
